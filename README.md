457af8f884c24631a80b1771c9d376e2782d4df9
Alan Palazzolo <apalazzolo@minnpost.com>
2013-04-15 12:06:36 -0500

    First draft.

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-jsonproxy
-=========
+# JSONProxy
 
 A Python Flask based proxy to turn JSON endpoints into JSONP for cross-domain requests.  Meant to be easily deployed to Heroku.  Allows for caching and whitelisting.
+
+## Similar applications
+
+There are a number of similar applications, but most of them do not whitelist of requests or cache results.  Whitelisting is important as a wide-open proxy can easily be abused and cause damage to your own application as well as others.  Caching adds a bit of performance boost.
+
+* http://jsonp.jit.su/
+* https://github.com/okfn/dataproxy
+* https://github.com/greenisus/jsonp-proxy
+* https://github.com/mvanholstyn/jsonp-proxy
+* https://github.com/clintandrewhall/node-jsonp-proxy
+* https://github.com/TomDemeranville/jsonprox
+
+## Install and run locally
+
+1. (optional) Create a virtualenv
+2. ```pip install -r requirements.txt```
+3. Set domain whitelist regex (defaults to Amazon's S3 ```s3\.amazonaws\.com$```):  ```export JSONPROXY_DOMAIN_WL_REGEX='<REGEX>'```
+3. Set path whitelist regex (defaults to open ```.```):  ```export JSONPROXY_PATH_WL_REGEX='<REGEX>'```
+4. Set how many minutes to keep cached values (default is ```1```): ```export JSONPROXY_CACHE=<MINUTES>```
+5. Run locally: ```python app.py```
+6. Go to http://localhost:5000
+
+## Deploy on Heroku
+
+For Heroku.
+
+1. Setup and install Heroku command line tools
+1. Create Heroku app with whatever name you want: ```heroku apps:create <APP_NAME>```
+3. Set domain whitelist regex (defaults to Amazon's S3 ```s3\.amazonaws\.com$```):  ```heroku config:set JSONPROXY_DOMAIN_WL_REGEX='<REGEX>'```
+3. Set path whitelist regex (defaults to open ```.```):  ```heroku config:set JSONPROXY_PATH_WL_REGEX='<REGEX>'```
+4. Set how many minutes to keep cached values (default is ```1```): ```heroku config:set JSONPROXY_CACHE=<MINUTES>```
+1. Push up code: ```git push heroku master```
+1. You can open the app with ```heroku open```
+1. Use in your application by making a request like the following.  Make sure to encode the proxy url parameter. ```http://<APP_NAME>.herokuapp.com/proxy?callback=<CALLBACK_NAME>&url=https%3A//http://jsonip.com/```
---
+++ app.py
@@ -0,0 +1,162 @@
+import os
+import json
+import datetime
+import re
+import urlparse
+import urllib
+import requests
+from flask import Flask, Response, render_template, request, abort
+from flask.ext.cache import Cache
+
+# Get environement variables
+debug_app = 'DEBUG_APP' in os.environ
+
+# Domain regex
+domain_regex = 's3\.amazonaws\.com$'
+if 'JSONPROXY_DOMAIN_WL_REGEX' in os.environ:
+  domain_regex = int(os.environ['JSONPROXY_DOMAIN_WL_REGEX'])
+domain_regex = re.compile(domain_regex, re.I)
+
+# Path regex
+path_regex = '.'
+if 'JSONPROXY_PATH_WL_REGEX' in os.environ:
+  path_regex = int(os.environ['JSONPROXY_PATH_WL_REGEX'])
+path_regex = re.compile(path_regex, re.I)
+
+# Cache time (in minutes)
+proxy_cache = 1
+if 'JSONPROXY_CACHE' in os.environ:
+  proxy_cache = int(os.environ['JSONPROXY_CACHE'])
+  
+  
+# Other application wide variables
+jsonp_header_overrides = {
+  'content-type': 'text/javascript; charset=UTF-8',
+  'Access-Control-Allow-Origin': '*'
+}
+
+# Make and configure the Flask app
+app = Flask(__name__)
+if debug_app:
+  app.debug = True
+
+
+# Set up cache
+cache_config = {
+  'CACHE_TYPE': 'filesystem',
+  'CACHE_THRESHOLD': 1000,
+  'CACHE_DIR': 'cache'
+}
+cache = Cache(config = cache_config)
+cache.init_app(app, config = cache_config)
+
+
+
+# Just a default route
+@app.route('/')
+@cache.cached(timeout = 500)
+def index():
+  return 'This is a whitelisted JSON to JSONP proxy.'
+    
+
+# Proxy route
+@app.route('/proxy')
+def handle_proxy():
+  request_url = request.args.get('url', '')
+  callback = request.args.get('callback', '')
+  request_parsed = urlparse.urlparse(request_url)
+  
+  # Check to make sure we have the right sort of request
+  if not request_url or not callback:
+    abort(404)
+
+  # Check if valid proxy url
+  if not is_valid_url(request_parsed):
+    abort(404)
+  
+  # Get value from proxied url (this is the cached part)
+  proxy_request = make_proxy(request_url)
+  if proxy_request['status_code'] != requests.codes.ok:
+    abort(proxy_request['status_code'])
+  
+  # Wrap callback
+  proxy_request['text'] = '%s(%s);' % (callback, proxy_request['text'])
+  
+  # Override some headers
+  proxy_request['headers'] = dict(proxy_request['headers'].items() + jsonp_header_overrides.items())
+  
+  return Response(proxy_request['text'], proxy_request['status_code'], proxy_request['headers'])
+
+
+# Get proxy URL and cache the results
+@cache.memoize(proxy_cache * 60)
+def make_proxy(url):
+  if app.debug:
+    print 'Cache missed: %s' % url
+
+  r = requests.get(url)
+  return {
+    'text': r.text,
+    'status_code': r.status_code,
+    'headers': r.headers,
+  }
+
+
+# Convert call to json request and get callback
+def convert_jsonp_to_json(url_parsed):
+  # Parses query string into list of tuples
+  query_parsed = urlparse.parse_qsl(url_parsed.query)
+
+  # Get callback if there is one
+  callback = False
+  for i, v in enumerate(query_parsed):
+    if query_parsed[i][0] == 'callback':
+      callback = query_parsed[i][1]
+      del query_parsed[i]
+  
+  # Convert jsonp request to json
+  jsonp_found = False
+  
+  for k in jsonp_to_json_keys:
+    for i, v in enumerate(query_parsed):
+      if query_parsed[i][0] == 'alt' and query_parsed[i][1] == k:
+        query_parsed[i] = ('alt', jsonp_to_json_keys[k])
+        jsonp_found = True
+  
+  # If no callback and jsonp, set default
+  if jsonp_found and not callback:
+    callback = default_google_callback
+  
+  # Recreate the query string by making new tuple.
+  # Note that we are reseting the fragment
+  # and any the other probably unnecessary parts.
+  new_url_tuple = url_parsed[0:4] + (urllib.urlencode(query_parsed),) + ('',)
+  
+  return urlparse.urlunparse(new_url_tuple), callback
+
+
+# Check if valid key is in url
+def is_valid_url(url_parsed):
+  # Make sure the the domain and path pass the
+  # check.
+  found = True
+  
+  # Check domain
+  domain_match = domain_regex.match(url_parsed.netloc)
+  if domain_match is None:
+    found = False
+  
+  # Check path
+  if found:
+    path_match = path_regex.match(url_parsed.path)
+    if path_match is None:
+      found = False
+
+  return found;
+
+
+# Start Flask App
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
